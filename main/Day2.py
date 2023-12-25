53f5a681fd0bbdeb7ffa3ee4ca719a3b3936438a
FaLL3n <94567401+FaLL3nWhizzy@users.noreply.github.com>
2023-12-26 02:22:34 +0530

    Create Day2.py

---
+++ main/Day2.py
@@ -0,0 +1,10 @@
+print("Getting to know you!")
+
+
+
+name = input("What is your name? ")
+favfood = input("What is your favorite food, " + name + " ?")
+favmusic = input("What is your favorite music, " + name + " ?")
+location = input ("Where do you live, " + name + " ?")
+
+print("You Are " + name + "\nYou're probably hungry for \n" + favfood + "\nyour fav music is " + favmusic + "\n You Must Be Going to " + location + " Keep Rocking")

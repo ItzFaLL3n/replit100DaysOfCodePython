1c6971103ec448518223fc3cbfcd69e912f911f3
FaLL3n <94567401+ItzFaLL3n@users.noreply.github.com>
2023-12-27 17:50:39 +0530

    Added New Scripts

---
+++ main/Day_3.py
@@ -0,0 +1,10 @@
+food = input("Name a Food : ")
+plant = input("Name a Plant : ")
+method = input("Name a Method of Coking : ")
+burntFood = input("What word describes burned food :")
+item = input("Name a household item : ")
+
+print()
+print("Tonight's Dinner!")
+
+print(method, food, "with", burntFood, plant, "on a bed of", item, ".")
---
+++ main/Day_4.py
@@ -0,0 +1,17 @@
+print("Welcome to your Adventure Story Simulator.")
+print ()
+print("I am going to ask you a bunch of questions and then create an epic story with you as the star.")
+
+
+name = input("What is your name? :")
+superpower = input("What is your superpower? :")
+favmusic = input("What's your favorite  Music? :")
+enemy = input("What is your worst enemy’s name?")
+food = input("Whats your favorite food? : ")
+where_you_live = input("Where do you live? :")
+
+print("Hello", name, "Your ability to", superpower, "will make sure you survive the battle. Go Listen to Your favorite music", favmusic, "While Walking down the streets of", where_you_live, "and use", superpower, "for good and not", "\033[31m" "evil!" )
+
+print()
+
+print("Whoa the python Colour", "\033[35m", "is pretty cool", "\033[0m")
